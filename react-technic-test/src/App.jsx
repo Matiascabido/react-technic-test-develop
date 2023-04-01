1291b1b38ed8c713dec23bcc3944278b479846ea
Matiascabido <cabidomatias@gmail.com>
2023-04-01 17:37:48 -0300

    Update const

--- react-technic-test/src/App.jsx
+++ react-technic-test/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import './App.css'
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 const CAT_ENDPOINT_RANDOM_IMAGES = 'https://cataas.com/cat/says/'
-const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
+const CAT_BASE_IMAGE_URL = 'https://cataas.com'
 
 export function App () {
   const [fact, setFact] = useState()
@@ -14,34 +14,33 @@ export function App () {
       .then(data => {
         const { fact } = data
         setFact(fact)
-
-        const firstWord = fact.split(' ', 3).join(' ')
-        console.log('FIRST WORD => ', firstWord)
-
-        fetch(`${CAT_ENDPOINT_RANDOM_IMAGES}/${firstWord}?size=50&color=red&json=true`)
-          .then(res => res.json())
-          .then(response => {
-            const { url } = response
-            setImageUrl(url)
-          })
       })
   }, [])
-  /* cada vez que se renderiza mi componente,
-  al tener los corchetes solo se ejecuta la primera verz */
+
+  useEffect(() => {
+    if (!fact) return
+    const firstWord = fact.split(' ', 3).join(' ')
+    fetch(`${CAT_ENDPOINT_RANDOM_IMAGES}/${firstWord}?size=50&color=red&json=true`)
+      .then(res => res.json())
+      .then(response => {
+        const { url } = response
+        setImageUrl(url)
+      })
+  }, [])
 
   return (
     <main>
       <h1> App de Gatitos </h1>
       {fact && <p>{fact}</p>}
       {imageUrl && <img
-        src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={
+        src={`${CAT_BASE_IMAGE_URL}${imageUrl}`} alt={
           `Image extracted using the first trhee words for ${fact}`
         }
                    />}
       <section>
         {fact && <p>{fact}</p>}
         {imageUrl && <img
-          src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={
+          src={`${CAT_BASE_IMAGE_URL}${imageUrl}`} alt={
           `Image extracted using the first trhee words for ${fact}`
         }
                      />}
